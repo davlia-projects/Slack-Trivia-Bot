67f098117c538760c229c8ef494b4eacbc5eb772
davlia <danedaworld@gmail.com>
2016-08-14 13:40:23 -0700

    refactor gameinstance into logic

--- bot/bot.go
+++ bot/bot.go
@@ -6,12 +6,13 @@ import (
 
 	c "github.com/dota-2-slack-bot/client"
 	"github.com/dota-2-slack-bot/config"
+	"github.com/dota-2-slack-bot/logic"
 
 	"github.com/nlopes/slack"
 )
 
 type Bot struct {
-	Channels map[string]*GameInstance
+	Channels map[string]*logic.GameInstance
 }
 
 var (
@@ -21,27 +22,25 @@ var (
 		HintDelay:    5,
 		QuestionTime: 30,
 	}
-	slackClient    *c.SlackClient              = c.GetSlackClient()
-	questionClient *c.QuestionClient           = c.GetQuestionClient()
-	params         slack.PostMessageParameters = slack.NewPostMessageParameters()
+	slackClient    *c.SlackClient    = c.GetSlackClient()
+	questionClient *c.QuestionClient = c.GetQuestionClient()
 )
 
 func NewBot() *Bot {
 	s := &Bot{
-		Channels: map[string]*GameInstance{},
+		Channels: map[string]*logic.GameInstance{},
 	}
 	return s
 }
 
 func (B *Bot) onStart() {
-	params.AsUser = true
 	channels, err := slackClient.API.GetChannels(true)
 	if err != nil {
 		log.Fatalf("error: could not retrieve list of channels (%+v)\n", err)
 	}
 	for _, channel := range channels {
 		if channel.IsMember {
-			B.Channels[channel.ID] = NewChannel(defaultConfig, channel.Name, channel.ID)
+			B.Channels[channel.ID] = logic.NewGameInstance(defaultConfig, channel.Name, channel.ID)
 		}
 	}
 }
@@ -60,8 +59,6 @@ func (B *Bot) HandleMessageEvent(ev *slack.MessageEvent) {
 		channel.ContinuousModeOff()
 	case "!s":
 		channel.GetStatsForPlayer(ev.User)
-	case "!debug":
-		slackClient.API.PostMessage(ev.Channel, "debug", params)
 	default:
 		if ev.SubType == "" {
 			channel.MakeGuess(ev.Text, ev.User)
@@ -71,7 +68,7 @@ func (B *Bot) HandleMessageEvent(ev *slack.MessageEvent) {
 
 func (B *Bot) HandleChannelJoinedEvent(ev *slack.ChannelJoinedEvent) {
 	fmt.Printf("Joined channel %s\n", ev.Channel.Name)
-	B.Channels[ev.Channel.ID] = NewChannel(defaultConfig, ev.Channel.Name, ev.Channel.ID)
+	B.Channels[ev.Channel.ID] = logic.NewGameInstance(defaultConfig, ev.Channel.Name, ev.Channel.ID)
 
 }
 
